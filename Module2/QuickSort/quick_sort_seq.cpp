e4a12a11e792aa3be3226ed2ca1c68c60188d57a
lachfoy <lachie.foy7@gmail.com>
2022-08-26 11:35:41 +1000

    seq quick sort

--- Module2/QuickSort/quick_sort_seq.cpp
+++ Module2/QuickSort/quick_sort_seq.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cstdlib> // rand, srand
+#include <ctime> // time
 
 using namespace std;
+
+int* RandomArray(const int array_size)
+{
+    int* array = new int[array_size];
+
+    for (int i = 0; i < array_size; i++)
+    {
+        array[i] = rand() % 100 + 1;
+    }
+
+    return array;
+}
 
 void PrintArray(const int* array, const int array_size)
 {
@@ -10,24 +24,66 @@ void PrintArray(const int* array, const int array_size)
     {
         cout << to_string(array[i]) << ","; 
     }
-    cout << to_string(array[array_size]) << "]\n"; // print the last item
+    cout << to_string(array[array_size - 1]) << "]\n"; // print the last item
+}
+
+int Partition(int* array, const int low, const int high)
+{
+    int pivot = array[high]; // choose the last element as the pivot
+
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(array[i], array[j]);
+        }
+    }
+
+    i++;
+    swap(array[i], array[high]);
+
+    return i;
+}
+
+void QuickSort(int* array, const int low, const int high)
+{
+    // base case
+    if (low >= high || low < 0) return;
+
+    // partition to get a pivot index
+    int p = Partition(array, low, high);
+
+    // recurse
+    QuickSort(array, low, p - 1); // left side
+    QuickSort(array, p + 1, high); // right side
 }
 
 int main() 
 {
+    // initialize rand with seed
+    srand(unsigned(time(0)));
+
     int array_size = 5;
-    int* array = new int[array_size];
 
-    array[0] = 3;
-    array[1] = 1;
-    array[2] = 5;
-    array[3] = 2;
-    array[4] = 4;
+    // create a randomized array
+    int* array = RandomArray(array_size);
 
-    cout << "Starting array: ";
+    cout << "before sort: ";
     PrintArray(array, array_size);
+
+    // sort the array
+    QuickSort(array, 0, array_size - 1);
+    
+    cout << "after sort: ";
+    PrintArray(array, array_size);
+
+    // free memory
+    delete[] array;
 
     return 0;
 }
 
-// g++ quick_sort_seq.cpp -o test
+// g++ quick_sort_seq.cpp -o test && ./test
